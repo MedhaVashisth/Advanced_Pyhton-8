62ba65f5d31441a809b8e5171b05acee4fc5a82c
MedhaVashisth <112161904+MedhaVashisth@users.noreply.github.com>
2022-10-03 17:46:43 +0530

    Add files via upload

---
+++ Advanced Assignment_Python 8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. Some commonly raised Exceptions are ArithmeticError, AttributeError, ImportError, IOError, FileNotFoundError, etc. Sometimes we must enforce constraints on the values that specific program variables can take or save the program from running into an undesired state.
+
+2. In Python, exceptions can be handled using a try statement. The critical operation which can raise an exception is placed inside the try clause. The code that handles the exceptions is written in the except clause. We can thus choose what operations to perform once we have caught the exception.
+
+3. In Python, exceptions can be handled using a try statement. The critical operation which can raise an exception is placed inside the try clause. The code that handles the exceptions is written in the except clause. We can thus choose what operations to perform once we have caught the exception.
+
+4. repr(e) gives you the exception(and the message string); str(e) only gives the message string. As an alternative for logging exception you could use logger. exception(e) instead. It will log the exception with traceback at the same logging.
+
+
+5. This is because a string is a sequence; we must assume that error messages are always more than two characters long!
